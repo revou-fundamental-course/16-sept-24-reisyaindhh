707f9e3437158173631c4d66df1d312434978d3a
REISYA INDAH KUSUMA <reisyaindah9@gmail.com>
2024-09-24 20:08:27 +0700

    first commit

--- java script/main.js
+++ java script/main.js
@@ -1,17 +1,4 @@
-// toggle & responsive nav
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navList = document.querySelector("nav");
-
-    burger.addEventListener("click", () => {
-        navList.classList.toggle("nav-active");
-        burger.classList.toggle("toggle-burger");
-    })
-}
-navSlide();
-
-// menghapus form setelah submit
-
+// menghapus form yg sudah di isi
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName ("form")) {
         form.reset();
@@ -19,3 +6,7 @@ window.onbeforeunload = () => {
 
 }
 
+let menuBtn = document.querySelector('#contact');
+let closeBtn = document.querySelector('#contact');
+
+// belum selesai hehhehe maaf
